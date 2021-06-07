c36b7a40c494f80caf45e9635ed66934e061d09e
Jose Maria Zamora Perez <jzamora@biada.net>
2021-06-07 13:32:30 +0200

    carga de personajes al hacer click

--- js/index.js
+++ js/index.js
@@ -1,9 +1,42 @@
 import getPersonajes from "./funciones.js";
 
-const personajes = document.querySelectorAll(".personajes");
-console.log(personajes);
-(async () => {
+const cargarPersonajes = document.querySelector(".cargar-personajes");
+const matarPersonajes = document.querySelector(".matar-familia");
+const buscarFamilia = document.querySelector(".matar-familia").value;
+const personajesPadre = document.querySelector(".personajes");
+const personaje = document.querySelectorAll(".personaje");
+const personajes = document.querySelector(".personaje-dummy");
+const limpiarPersonajes = () => {
+  for (const elemento of personaje) {
+    elemento.classList.add("personaje-dummy");
+  }
+};
+limpiarPersonajes();
+const cargaPersonaje = cargarPersonajes.addEventListener("click", async () => {
   const response = getPersonajes;
-  console.log(response);
   const datos = await response.then((dato) => dato);
-})();
+  console.log(datos);
+  limpiarPersonajes();
+  for (const { nombre, familia, vivo } of datos) {
+    const moldePersonaje = personajes.cloneNode(true);
+    moldePersonaje.classList.remove("personaje-dummy");
+    const nombrePersonaje = moldePersonaje.querySelector(".nombre");
+    const familiaPersonaje = moldePersonaje.querySelector(".familia");
+    const estado = moldePersonaje.querySelector(".estado");
+    nombrePersonaje.textContent = nombre;
+    familiaPersonaje.textContent = familia;
+    estado.textContent = vivo ? "vivo" : "muerto";
+    personajesPadre.append(moldePersonaje);
+  }
+
+  /* const matarFamilia = (personajes, familia) => {
+    const personajeDeFamilia = personajes.filter(
+      (personaje) => personaje.famila === familia
+    );
+    for (const personaje of personajeDeFamilia) {
+      personaje.vivo = false;
+    }
+  };
+  matarFamilia("click", matarFamilia(datos, buscarFamilia));
+  console.log(datos); */
+});
